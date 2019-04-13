efe78f4b19d0880b308c15cacfdbde9ff523a77b
RSandhir <sandhirr2d2@gmail.com>
2019-04-13 22:10:16 +0530

    removed snackbar import

--- app/src/main/java/com/rbi/customprogressbar/MainActivity.java
+++ app/src/main/java/com/rbi/customprogressbar/MainActivity.java
@@ -1,11 +1,9 @@
 package com.rbi.customprogressbar;
 
 import android.database.Cursor;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
